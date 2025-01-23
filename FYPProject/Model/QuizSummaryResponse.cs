8e6c04fd8380950ea4d68b9a9e16155bafeb637f
DARYL LAI YONG TAI RP <22014780@myrp.edu.sg>
2025-01-23 12:39:13 +0800

    Added QuizSummaryResponse.cs to model folder. -Adrian

---
+++ FYPProject/Model/QuizSummaryResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace FYPProject.Model
+{
+    public class QuizSummaryResponse
+    {
+        public string QuestionText { get; set; }
+        public string AnswerText { get; set; }
+        public bool IsCorrect { get; set; }
+        public double Score { get; set; }
+    }
+
+}
