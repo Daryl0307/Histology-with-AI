ebf4e33bb8cdd42b816845a49f850e7dc8a5f2ac
DARYL LAI YONG TAI RP <22014780@myrp.edu.sg>
2025-01-23 12:32:25 +0800

    Added HistoQuizViewModel and QuizStatistics to model folder -Adrian

---
+++ FYPProject/Model/HistoQuizViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace FYPProject.Model
+{
+    public class HistoQuizViewModel
+    {
+        public List<HistoQuiz> HistoQuiz { get; set; }
+        public List<QuizStatistics> QuizStatistics { get; set; }
+    }
+
+}
---
+++ FYPProject/Model/QuizStatistics.cs
@@ -0,0 +1,12 @@
+﻿namespace FYPProject.Model
+{
+    public class QuizStatistics
+    {
+        public int UserId { get; set; }
+        public string QuizCategory { get; set; }
+        public DateTime DateAttempted { get; set; }
+        public double Score { get; set; }
+
+    }
+
+}
