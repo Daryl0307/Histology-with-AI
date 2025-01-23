981933aed33647466246d086c71ccb4b2a78e971
DARYL LAI YONG TAI RP <22014780@myrp.edu.sg>
2025-01-23 12:35:57 +0800

    Added Question to model folder -Adrian

---
+++ FYPProject/Model/Question.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project.Model
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        [Required(ErrorMessage = "The QuestionText field is required.")]
+        public string QuestionText { get; set; }
+        public string QuestionType { get; set; }
+        public double QuestionMark { get; set; }
+        public int QuizId { get; set; } // Foreign Key
+
+
+    }
+
+}
