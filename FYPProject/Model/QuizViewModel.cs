578d61b1891a34b9c2acda9bbb0eb287fef4a59e
DARYL LAI YONG TAI RP <22014780@myrp.edu.sg>
2025-01-23 12:40:54 +0800

    Added QuizViewModel.cs to model folder -Adrian

---
+++ FYPProject/Model/QuizViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace FYPProject.Model
+{
+    public class QuizViewModel
+    {
+        public Quiz Quiz { get; set; }
+        public Question Question { get; set; }
+        public List<Answer> Answer { get; set; } = new List<Answer>();
+        public Photo? Photo { get; set; }
+    }
+}
