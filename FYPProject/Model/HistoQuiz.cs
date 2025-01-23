b09ab7702b8f9d87dcfcece45feb8b7c67d89207
DARYL LAI YONG TAI RP <22014780@myrp.edu.sg>
2025-01-23 12:29:50 +0800

    Added HistoQuiz.cs to Model folder. -Adrian

---
+++ FYPProject/Model/HistoQuiz.cs
@@ -0,0 +1,9 @@
+﻿namespace FYPProject.Model
+{
+    public class HistoQuiz
+    {
+        public string QuizCategory { get; set; }
+        public double TotalQuestionMarks { get; set; }
+
+    }
+}
