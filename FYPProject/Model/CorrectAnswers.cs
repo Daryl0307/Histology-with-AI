126acb45d6c67556468f6dcc28e0a80be557167e
Adrian Heng <22010079@myrp.edu.sg>
2025-01-28 11:48:18 +0800

    Added CorrectAnswers and QuizSummaryViewModel Model

---
+++ FYPProject/Model/CorrectAnswers.cs
@@ -0,0 +1,10 @@
+﻿namespace FYPProject.Model
+{
+    public class CorrectAnswers
+    {
+        public string QuizCategory { get; set; }
+        public int QuestionId { get; set; }
+        public string GroupedAnswerText { get; set; }
+        public int IsCorrect { get; set; }
+    }
+}
---
+++ FYPProject/Model/QuizSummaryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FYPProject.Model
+{
+    public class QuizSummaryViewModel
+    {
+        public List<QuizSummaryResponse> QuizSummaryResponse { get; set; }
+        public List<CorrectAnswers> CorrectAnswewrs { get; set; }
+    }
+}
